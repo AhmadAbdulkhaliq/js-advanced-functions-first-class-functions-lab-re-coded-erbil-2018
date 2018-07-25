8b3fd651442243d6fb99c1d91e096a619fc94196
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-25 11:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
   const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-const FirstTwoDrivers= (function (array){ 
+const returnFirstTwoDrivers= (function (array){ 
  return array.slice(0,2)
 
 })
