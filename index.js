66d14a20e6dd2da8f2bf7cf009fd82942a060128
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-25 11:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
   const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-const returnFirstTwoDrivers=(function (drivers){ return  })()
+const returnFirstTwoDrivers= (function (drivers){ return  })()
